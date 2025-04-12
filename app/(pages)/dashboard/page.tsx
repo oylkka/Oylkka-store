8d06c2956996ed40c8eeb072fb555c0dcd527d5f
sejarparvez <sejarparvez@yahoo.com>
2025-04-12 20:40:06 +0600

    added new product page

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { auth } from '@/auth';
+
+export default async function Page() {
+  const session = await auth();
+  return (
+    <>
+      {session?.user.role === 'ADMIN' ? (
+        <p>Admin dashboard</p>
+      ) : (
+        <p>User dashboard</p>
+      )}
+      <div className="flex flex-1 flex-col gap-4">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+        </div>
+        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      </div>
+    </>
+  );
+}
